a15103a490a07414954b1b5978ef75ac925a4cf0
Thiago Duarte <thiago-duarte@outlook.com>
2017-04-21 18:13:29 -0300

    Teste de publicacao

--- config/express.js
+++ config/express.js
@@ -41,6 +41,11 @@ module.exports = function()
 	.then('controllers')
 	.then('routes')
 	.into(app);
+	
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 
 	return app;
 };
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-var http = require('http');
-var app = require('./config/express')();
 require('./config/database.js')('mongodb://localhost/gurudb');
+require('./config/express')();
+
+
+/*
 
 http.createServer(app).listen(app.get('port'),app.get('host'),function(){
     console.log('Express running in port: ' + app.get('port'));
 	console.log('Express running in host: ' + app.get('host'));
     
 });
-/*
 var express = require('express');
 var app = express();
 
