d8b34258ef18d9dd36e89a1fe0b63468cd88aa58
Thiago Duarte <thiago-duarte@outlook.com>
2017-04-21 18:09:33 -0300

    Teste de publicacao

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+var http = require('http');
+var app = require('./config/express')();
+require('./config/database.js')('mongodb://localhost/gurudb');
+
+http.createServer(app).listen(app.get('port'),app.get('host'),function(){
+    console.log('Express running in port: ' + app.get('port'));
+	console.log('Express running in host: ' + app.get('host'));
+    
+});
+/*
 var express = require('express');
 var app = express();
 
@@ -12,5 +22,4 @@ app.set('view engine', 'ejs');
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
+*/
